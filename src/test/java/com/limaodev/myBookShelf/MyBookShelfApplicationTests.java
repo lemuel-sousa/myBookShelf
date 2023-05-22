74c957bad012e6055fbd4a1ec6bc09a6636eac29
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-22 16:52:25 -0300

    initial commit

---
+++ src/test/java/com/limaodev/myBookShelf/MyBookShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limaodev.myBookShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
