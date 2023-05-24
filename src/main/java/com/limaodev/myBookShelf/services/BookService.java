ead4a3f02b0e01a8517b216e2070f3bfc9a20cc8
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-24 09:50:19 -0300

    updated anotations

--- src/main/java/com/limaodev/myBookShelf/services/BookService.java
+++ src/main/java/com/limaodev/myBookShelf/services/BookService.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.limaodev.myBookShelf.DTO.BookBasicDTO;
 import com.limaodev.myBookShelf.entities.Book;
 import com.limaodev.myBookShelf.repositories.BookRepository;
 
-import jakarta.transaction.Transactional;
 
 @Service
 public class BookService {
